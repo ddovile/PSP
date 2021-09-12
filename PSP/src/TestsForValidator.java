70acdd91341df90a80f53b6161fa0bffe061a842
ddovile <71456312+ddovile@users.noreply.github.com>
2021-09-12 17:47:42 +0300

    small fix

--- PSP/src/TestsForValidator.java
+++ PSP/src/TestsForValidator.java
@@ -1,7 +1,8 @@
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
-import org.junit.jupiter.api.*;
 public class TestsForValidator 
 {
     Validator validator;
