fc8d7232efcbac4260328e4141c09b7488de80e9
ddovile <71456312+ddovile@users.noreply.github.com>
2021-10-02 22:51:18 +0300

    Add files via upload

---
+++ ValidationModule-4b3b7567d8ed306e20ddb1fee6e302fafb6a1837/src/EmailValidator.java
@@ -0,0 +1,14 @@
+public class EmailValidator {
+
+    public boolean checkForAtSymbol(String email) {
+        return false;
+    }
+
+    public boolean checkForIllegalSymbols(String email) {
+        return false;
+    }
+
+    public boolean checkDomainAndTLD(String email) {
+        return false;
+    }
+}
---
+++ ValidationModule-4b3b7567d8ed306e20ddb1fee6e302fafb6a1837/src/PasswordChecker.java
@@ -0,0 +1,14 @@
+public class PasswordChecker {
+
+    public boolean checkPasswordLength(String pass, int minLen) {
+        return false;
+    }
+
+    public boolean checkPasswordForUppercase(String pass) {
+        return false;
+    }
+
+    public boolean checkPasswordForSpecialSymbols(String pass) {
+        return false;
+    }
+}
